3b4eeaa36eed78da9430d1f38f6cffe771c5b5cf
ivoamaro <33934913+ivoamaro@users.noreply.github.com>
2026-02-19 18:43:25 +0000

    add astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import lenis from "astro-lenis";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [lenis()],
+});

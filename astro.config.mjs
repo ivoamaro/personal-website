a28efc09e6a8a4382378f045a7d7f299f48baf43
Ivo Amaro <33934913+ivoamaro@users.noreply.github.com>
2026-02-20 09:51:38 +0000

    remove lenis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import lenis from "astro-lenis";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [lenis()],
-});
+export default defineConfig({});

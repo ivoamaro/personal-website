14304c26c0850d7eb627b99fb73df1649fcc559b
Ivo Amaro <33934913+ivoamaro@users.noreply.github.com>
2026-04-01 21:30:55 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+    projectId: 'ezatuf8z',
+    dataset: 'production',
+    useCdn: true,
+  }), react()]
+});

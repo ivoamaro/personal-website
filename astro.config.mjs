deb1b459fa4e6747e93953d162962861264851e6
Ivo Amaro <33934913+ivoamaro@users.noreply.github.com>
2026-04-02 09:59:25 +0100

    sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
 
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+const env = loadEnv(import.meta.env.MODE, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
-    projectId: 'ezatuf8z',
-    dataset: 'production',
+    projectId: env.PUBLIC_SANITY_PROJECT_ID,
+    dataset: env.PUBLIC_SANITY_DATASET,
     useCdn: true,
   }), react()]
 });

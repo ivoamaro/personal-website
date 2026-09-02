e09378edabdcb57b61ea6b5c3bfe7f30d5129dad
ivoamaro <33934913+ivoamaro@users.noreply.github.com>
2026-09-02 11:55:44 +0100

    image fix for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,11 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  image: {
+    endpoint: {
+      route: "/_image",
+      entrypoint: "@astrojs/cloudflare/image-endpoint"
+    }
+  },
   adapter: cloudflare(),
 });

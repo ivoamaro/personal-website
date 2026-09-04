c832b9f293ebbe752f64e215f8d57f4b4e7bb68d
ivoamaro <33934913+ivoamaro@users.noreply.github.com>
2026-09-04 10:20:40 +0100

    add seo, sitemap, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+
+
 
 export default defineConfig({
+  site: 'https://ivoamaro.com',
   image: {
     endpoint: {
       route: "/_image",
       entrypoint: "@astrojs/cloudflare/image-endpoint"
     }
   },
+
   adapter: cloudflare(),
+  integrations: [robotsTxt(), sitemap()],
 });
